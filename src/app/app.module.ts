0b1dbc272def117e8e6638f0daa189facbc5664b
Stiveouissant <olek.busek@gmail.com>
2020-06-18 20:23:42 +0200

    Last commit - bye bye

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 
+
 @NgModule({
   declarations: [
     AppComponent,
